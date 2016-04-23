ad1b06dc2fdf57100d55b3dd49641dfb73ebbc8c
Robb Shecter <robb@weblaws.org>
2016-04-23 15:40:26 -0700

    Added APP_ID
    This finishes the first pass - it might work!

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@
 /**
  * App ID for the skill
  */
-var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var APP_ID = 'arn:aws:lambda:us-east-1:014157332264:function:complimentBot';
 
 /**
  * Array containing space facts.
